aa09b00ae3f52de84ec7be65bc5ad4889e74d2e6
xMansour <MEMansour96@gmail.com>
2022-09-07 12:03:51 +0200

    Controller setup

---
+++ src/main/java/com/mansourappdevelopment/jdbcjavafxcrud/controllers/HelloController.java
@@ -0,0 +1,58 @@
+package com.mansourappdevelopment.jdbcjavafxcrud.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextField;
+
+public class HelloController {
+    @FXML
+    private TextField idInput;
+    @FXML
+    private TextField titleInput;
+    @FXML
+    private TextField authorInput;
+    @FXML
+    private TextField yearInput;
+    @FXML
+    private TextField pagesInput;
+    @FXML
+    private Button createBtn;
+    @FXML
+    private Button updateBtn;
+    @FXML
+    private Button deleteBtn;
+    @FXML
+    private Button commitBtn;
+    @FXML
+    private Button rollbackBtn;
+    @FXML
+    private TableColumn idColumn;
+    @FXML
+    private TableColumn titleColumn;
+    @FXML
+    private TableColumn authorColumn;
+    @FXML
+    private TableColumn yearColumn;
+    @FXML
+    private TableColumn pagesColumn;
+
+    public void onCreateClicked(ActionEvent actionEvent) {
+
+    }
+
+
+    public void onUpdateClicked(ActionEvent actionEvent) {
+    }
+
+    public void onDeleteClicked(ActionEvent actionEvent) {
+    }
+
+    public void onCommitClicked(ActionEvent actionEvent) {
+    }
+
+    public void onRollbackClicked(ActionEvent actionEvent) {
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,8 +3,8 @@ module com.mansourappdevelopment.jdbcjavafxcrud {
     requires javafx.fxml;
 
     requires org.controlsfx.controls;
-    requires org.kordamp.bootstrapfx.core;
-
     opens com.mansourappdevelopment.jdbcjavafxcrud to javafx.fxml;
     exports com.mansourappdevelopment.jdbcjavafxcrud;
+    exports com.mansourappdevelopment.jdbcjavafxcrud.controllers;
+    opens com.mansourappdevelopment.jdbcjavafxcrud.controllers to javafx.fxml;
 }
